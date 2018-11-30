082299c1e1b260af3dabbb738122087d1a287c2b
eyob tariku <eyobtariku@gmail.com>
2018-12-01 01:27:25 +0300

    [#1] - Create a new app called `listings`
    * Add the app to `INSTALLED_APPS` list
    * Add a URL pattern to the app

--- config/settings/base.py
+++ config/settings/base.py
@@ -41,8 +41,9 @@ INSTALLED_APPS += [
 
 # Project apps
 INSTALLED_APPS += [
-    'accounts',
-    'core',
+    'accounts.apps.AccountsConfig',
+    'core.apps.CoreConfig',
+    'listings.apps.ListingsConfig',
 ]
 
 MIDDLEWARE = [
